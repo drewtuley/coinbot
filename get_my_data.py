1603afaef99b424ae80828b7b0d69008ff944500
Andrew Tuley <drewtuley@gmail.com>
2017-08-03 10:28:55 +0100

    add total GBP valuation to get_my_data

--- get_my_data.py
+++ get_my_data.py
@@ -10,10 +10,18 @@ if __name__ == '__main__':
     balance = cb.get_balance()
     print('My Balance: \n{}'.format(balance))
 
+    mo, resp = cb.estimate_market('sell', {'quantity': balance.xbt_available})
+    if mo is not None:
+        print('GBP cash valuation: {} @ {}'.format(mo, mo.price()))
+        print('Total Cash Valuation: {}'.format(mo.total + balance.gbp_balance))
+    else:
+        print('unable to get estimate sell market: status: {} value {}'.format(resp.status_code, resp.value))
+
     open_orders = cb.get_open_orders()
     print('My Open Orders:')
     for open_order in open_orders:
         print(open_order)
+
 
     txns_to_show = 10
     if len(sys.argv) > 1:
