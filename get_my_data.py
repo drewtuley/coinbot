887071186a3d50e9fb106523dfebba305f09b544
Andrew Tuley <drewtuley@gmail.com>
2017-08-10 14:57:54 +0100

    only get estimated value if +ve XBT balance

--- get_my_data.py
+++ get_my_data.py
@@ -10,12 +10,13 @@ if __name__ == '__main__':
     balance = cb.get_balance()
     print('My Balance: \n{}'.format(balance))
 
-    mo, resp = cb.estimate_market('sell', {'quantity': balance.xbt_available})
-    if mo is not None:
-        print('GBP cash valuation: {} @ {}'.format(mo, mo.price()))
-        print('Total Cash Valuation: {}'.format(mo.total + balance.gbp_balance))
-    else:
-        print('unable to get estimate sell market: status: {} value {}'.format(resp.status_code, resp.value))
+    if balance.xbt_available > 0:
+        mo, resp = cb.estimate_market('sell', {'quantity': balance.xbt_available})
+        if mo is not None:
+            print('GBP cash valuation: {} @ {}'.format(mo, mo.price()))
+            print('Total Cash Valuation: {}'.format(mo.total + balance.gbp_balance))
+        else:
+            print('unable to get estimate sell market: status: {} value {}'.format(resp.status_code, resp.value))
 
     open_orders = cb.get_open_orders()
     print('My Open Orders:')
