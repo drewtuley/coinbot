e3119eca0023f099228f57b973515409d626cdba
Andrew Tuley <drewtuley@gmail.com>
2017-08-24 10:58:46 +0100

    impoved valuation displays

--- get_my_data.py
+++ get_my_data.py
@@ -6,11 +6,14 @@ from CoinfloorBot import UserTransaction
 if __name__ == '__main__':
     cb = CoinfloorBot()
     cb.set_config('coinfloor.props')
+    session = cb.get_db_session(echo=False)
 
     current_xbt_price = None
 
     balance = cb.get_balance()
     print('My Balance: \n{}'.format(balance))
+    session.add(balance)        # update to db
+    session.commit()
 
     if balance.xbt_available > 0:
         mo, resp = cb.estimate_market('sell', {'quantity': balance.xbt_available})
@@ -33,17 +36,6 @@ if __name__ == '__main__':
 
     print ('My Transactions (top {})'.format(txns_to_show))
     user_txns = cb.get_user_transactions()
-    gbp_balance = balance.gbp_balance
-    xbt_balance = balance.xbt_balance
-    for txn in user_txns[:txns_to_show]:
-        if current_xbt_price is not None:
-            val = current_xbt_price * txn.xbt
-            pnl = val + txn.gbp
-        else:
-            val = None
-        print('{txn} Val:{val:8.2f} PnL:{pnl:8.2f}'.format(txn=txn, val=val, pnl=pnl))
-
-    session = cb.get_db_session(echo=False)
     for txn in sorted(user_txns, key=lambda UserTransaction: UserTransaction.tid):
         txn_exists = session.query(UserTransaction).filter_by(tid=txn.tid).first()
         if txn_exists is None:
@@ -51,9 +43,17 @@ if __name__ == '__main__':
 
     session.commit()
 
-    session.add(balance)
-   # print(session.new)
-    session.commit()
+    gbp_balance = balance.gbp_balance
+    xbt_balance = balance.xbt_balance
+    db_user_txns = session.query(UserTransaction).order_by(UserTransaction.tid.desc())
+    for txn in db_user_txns[:txns_to_show]:
+        if current_xbt_price is not None:
+            val = current_xbt_price * txn.xbt
+            pnl = val + txn.gbp
+        else:
+            val = 0.0
+            pnl = 0.0
+        print('{txn} Val:{val:8.2f} PnL:{pnl:8.2f}'.format(txn=txn, val=val, pnl=pnl))
 
     #our_txn = session.query(UserTransaction).filter_by(tid='1496828041313320').first()  # doctest:+NORMALIZE_WHITESPACE
     #print(our_txn)
