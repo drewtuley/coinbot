7e2bb3657df0f3cbf333d33a856699120922f2a9
Andrew Tuley <drewtuley@gmail.com>
2017-10-03 09:20:14 +0100

    post my host & internet IP to slack every hour or so

--- ticker.py
+++ ticker.py
@@ -1,7 +1,10 @@
 import time
-
+import requests
+import re
+import os
 from CoinfloorBot import CoinfloorBot
 from CoinfloorBot import Ticker
+from expiringdict import ExpiringDict
 
 
 def show_change(curr_val, prev_val):
@@ -15,7 +18,17 @@ def show_change(curr_val, prev_val):
     return '{val} {arrow}'.format(val=curr_val, arrow=arrow)
 
 
+def get_my_ip():
+    IP='http://ip4.me'
+    r = requests.get(IP)
+    if r.status_code == 200:
+        m=re.search('\d+[.]\d+[.]\d+[.]\d+', r.text)
+        if m != None:
+            return (m.group())
+
+
 if __name__ == '__main__':
+    cache = ExpiringDict(max_len=100, max_age_seconds=3600)
     cb = CoinfloorBot()
     cb.set_config('coinfloor.props')
 
@@ -36,6 +49,7 @@ if __name__ == '__main__':
                 else:
                     val = 0
                     valdiff = 0
+
                 message = '{dt} bid: {bid} ask: {ask} - last: {last}  vol(24H): {vol:3.2f} ({vc:2.2f}) val:({val} {valdiff:+3.2f})' \
                     .format(dt=t.date, bid=show_change(t.bid, prev_t.bid), ask=show_change(t.ask, prev_t.ask),
                             last=show_change(t.last, prev_t.last), vol=t.volume, vc=volchange, val=val, valdiff=valdiff)
@@ -43,6 +57,11 @@ if __name__ == '__main__':
                 session.add(t)
                 session.commit()
                 prev_t = t
+
+                if 'myip' not in cache:
+                    myip = get_my_ip()
+                    cb.post_to_slack('ticker connected on {} ({})'.format(os.uname()[1], myip))
+                    cache['myip'] = myip
         except Exception as err:
             print(err)
             pass
