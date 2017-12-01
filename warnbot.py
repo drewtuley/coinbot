029168939aec1a62b22264197c2a7153ffa47957
drewtuley <drewtuley@gmail.com>
2017-12-01 11:32:22 +0000

    major reformatting & tidying courtesy of PyCharm :)

--- warnbot.py
+++ warnbot.py
@@ -1,10 +1,8 @@
-import os
 import time
 from slackclient import SlackClient
 from CoinfloorBot import CoinfloorBot
 import ConfigParser
 from parse import *
-
 
 cb = CoinfloorBot()
 cb.set_config('coinfloor.props')
@@ -12,16 +10,24 @@ my_parser = ConfigParser.SafeConfigParser()
 my_parser.read('coinfloor.props')
 
 # warnbot's ID in config file
-bot_id = my_parser.get('warnbot','bot_id')
-slack_bot_token = my_parser.get('warnbot','slack_bot_token')
+bot_id = my_parser.get('warnbot', 'bot_id')
+slack_bot_token = my_parser.get('warnbot', 'slack_bot_token')
 
 # constants
 AT_BOT = "<@" + bot_id + ">"
 HELP = 'help'
-HELP_MESSAGE = '```Usage:\n    @warnbot when <coins> [above|below|changes] <value>\n    @warnbot list\n    @warnbot clear\n\nExamples:\n    @warnbot when 1 above 7000   # warn me when the price of 1 XBT rises above 7000 GBP\n    @warnbot when 1.5 below 7890   # warn me when 1.5 XBT prices above 7890 GBP\n    @warnbot when 2.3 changes -5    # warn me when the price of 2.3XBT drops by 5%```'
+HELP_MESSAGE = '```Usage:\n    @warnbot when <coins> [above|below|changes] <value>\n    @warnbot list\n' \
+               '    @warnbot clear\n\nExamples:\n' \
+               '    @warnbot when 1 above 7000   # warn me when the price of 1 XBT rises above 7000 GBP\n' \
+               '    @warnbot when 1.5 below 7890   # warn me when 1.5 XBT prices above 7890 GBP\n' \
+               '    @warnbot when 2.3 changes -5    # warn me when the price of 2.3XBT drops by 5%```'
+
+HWM = 1
+LWM = 2
+watermark_msgs = {HWM: 'rises above', LWM: 'falls below'}
 
 # hash of 'value' requests by user
-warnings={}
+warnings = {}
 warning_count = 3
 
 # instantiate Slack & Twilio clients
@@ -31,9 +37,9 @@ slack_client = SlackClient(slack_bot_token)
 def get_coin_value(amount):
     mo, resp = cb.estimate_market('sell', {'quantity': amount})
     if mo is not None:
-        text = '{}'.format(mo.total)
-
-    return float(text)
+        return mo.total
+    else:
+        return None
 
 
 def get_values_a(p):
@@ -42,46 +48,45 @@ def get_values_a(p):
         value = float(p['value'])
         return coins, value
     except:
-       return None, None
-   
-def register_hwm(p, channel, user, ts):
+        return None, None
+
+
+def get_register_msg(coins, watermark, value):
+    return 'registered a warning when the value of {coins} XBT {bar} {value} GBP'.format(coins=coins,
+                                                                                         bar=watermark_msgs[watermark],
+                                                                                         value=value)
+
+
+def register_watermark(watermark, p, channel, user, ts):
     coins, value = get_values_a(p)
-    if coins == None:
+    if coins is None:
         return None
-    warnings[ts] = (1, channel, user, coins, value, warning_count)
-    response='registered a warning when the value of {} XBT rises above {} GBP'.format(coins, value)
-
-    return response
-
-
-def register_lwm(p, channel, user, ts):
-    coins, value = get_values_a(p)
-    if coins == None:
-        return None
-    warnings[ts] = (2, channel, user, coins, value, warning_count)
-    response='registered a warning when the value of {} XBT drops below {} GBP'.format(coins, value)
+    warnings[ts] = (watermark, channel, user, coins, value, warning_count)
+    response = get_register_msg(coins, watermark, value)
 
     return response
 
 
 def register_change(p, channel, user, ts):
     coins, pcchange = get_values_a(p)
-    if coins == None:
+    if coins is None:
         return None
     rtvalue = get_coin_value(coins)
-    value = rtvalue + (rtvalue * (pcchange/100.0))
-    if pcchange > 0:
-        warnings[ts] = (1, channel, user, coins, value, warning_count)
-        response='registered a warning when the value of {} XBT rises above {} GBP'.format(coins, value)
-    else:
-        warnings[ts] = (2, channel, user, coins, value, warning_count)
-        response='registered a warning when the value of {} XBT drops below {} GBP'.format(coins, value)
+    if rtvalue is not None:
+        value = rtvalue + (rtvalue * (pcchange / 100.0))
+        if pcchange > 0:
+            warnings[ts] = (HWM, channel, user, coins, value, warning_count)
+            response = get_register_msg(coins, HWM, value)
+        else:
+            warnings[ts] = (LWM, channel, user, coins, value, warning_count)
+            response = get_register_msg(coins, HWM, value)
+        return response
 
-    return response
+    return None
 
 
 def clear_warnings(user):
-    tozap=[]
+    tozap = []
     for ts in warnings:
         if warnings[ts][2] == user:
             tozap.append(ts)
@@ -94,50 +99,44 @@ def clear_warnings(user):
 def register_warning(command, channel, user, ts):
     print(command)
 
-    response=HELP_MESSAGE
-    coins = None
-    value = None
+    response = HELP_MESSAGE
     p = parse('when {coins} below {value}', command)
     if p:
-        r = register_lwm(p, channel, user, ts)
-        if r != None:
+        r = register_watermark(LWM, p, channel, user, ts)
+        if r is not None:
             response = r
     p = parse('when {coins} above {value}', command)
     if p:
-        r = register_hwm(p, channel, user, ts)
-        if r != None:
+        r = register_watermark(HWM, p, channel, user, ts)
+        if r is not None:
             response = r
     p = parse('when {coins} changes {value}', command)
     if p:
         r = register_change(p, channel, user, ts)
-        if r != None:
+        if r is not None:
             response = r
 
     return response
 
 
 def show_warnings(iuser):
-    msg=''
+    msg = ''
     idx = 1
     for ts in warnings:
         warntype, channel, user, coins, value, count = warnings[ts]
         if iuser == user:
-            if warntype == 1:
-                does='rises above'
-            else:
-                does='drops below'
-            msg = msg + '{}: warn me when the value of {} XBT {} {} GBP\n'.format(idx, coins, does, value)
+            msg = msg + '{idx}: {msg}\n'.format(idx, get_register_msg(coins, warntype, value))
             idx += 1
 
     if msg != '':
-        return '```'+msg+'```'
+        return '```' + msg + '```'
     else:
         return ''
 
 
 def set_repeat(command):
     response = 'invalid repeat command: {}'.format(command)
-    p = parse('repeat {times}',command)
+    p = parse('repeat {times}', command)
     if p:
         try:
             warnings = int(p['times'])
@@ -149,28 +148,28 @@ def set_repeat(command):
 
 
 def process_warnings():
-    tozap=[]
+    tozap = []
     for ts in warnings:
         print(warnings[ts])
         resp = None
         warntype, channel, user, coins, value, count = warnings[ts]
         rtvalue = get_coin_value(coins)
         print('value of {} is {}'.format(coins, rtvalue))
-        if warntype == 1 and rtvalue > value:
+        if warntype == HWM and rtvalue > value:
             resp = 'Warning <@{}>: value of {} XBT has risen above {} to {}'.format(user, coins, value, rtvalue)
             count -= 1
-        elif warntype == 2 and rtvalue < value:
+        elif warntype == LWM and rtvalue < value:
             resp = 'Warning <@{}>: value of {} XBT has dropped below {} to {}'.format(user, coins, value, rtvalue)
             count -= 1
         if count > 0:
             warnings[ts] = (warntype, channel, user, coins, value, count)
         else:
             tozap.append(ts)
- 
-        if resp !=None:
+
+        if resp is not None:
             slack_client.api_call("chat.postMessage", channel=channel,
-                          text=resp, as_user=True)
-  
+                                  text=resp, as_user=True)
+
     for ts in tozap:
         warnings.pop(ts)
 
@@ -198,7 +197,6 @@ def handle_command(command, channel, user, ts):
                           text=response, as_user=True)
 
 
-
 def parse_slack_output(slack_rtm_output):
     """
         The Slack Real Time Messaging API is an events firehose.
@@ -220,7 +218,7 @@ def parse_slack_output(slack_rtm_output):
 
 if __name__ == "__main__":
 
-    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    READ_WEBSOCKET_DELAY = 1  # 1 second delay between reading from firehose
     if slack_client.rtm_connect():
         print("warnbot connected and running!")
         while True:
